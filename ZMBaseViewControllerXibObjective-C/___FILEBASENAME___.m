fb2ea2b848683c9101680a3f941df96efe7b2f50
CHT-Technology <cht@CHT-TechnologydeiMac.local>
2017-05-23 16:23:04 +0800

    first commit

---
+++ ZMBaseViewControllerXibObjective-C/___FILEBASENAME___.m
@@ -0,0 +1,54 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#import "___FILEBASENAME___.h"
+
+@interface ___FILEBASENAMEASIDENTIFIER___ ()
+
+@end
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+#pragma mark -- Life Circle
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    [self setupUI];
+    
+    [self setupData];
+    
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+}
+
+#pragma mark -- Lazzy
+
+#pragma mark -- Private Methods
+//配置视图
+-(void)setupUI{
+    
+}
+
+//配置数据
+-(void)setupData{
+    
+}
+
+#pragma mark -- Target Actions
+
+
+@end
